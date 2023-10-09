0e46cc85820b9e8955f0876ea06f43a71adccb79
Marcio A. M. Reyes <>
2023-10-09 01:52:22 -0300

    muitos templates e teste

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, template_render, request, flash
+from flask import Flask, render_template, request, flash
+import random
 
 app = Flask(__name__)
 app.secretkey = "mrproductions"
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return template_render('index.html')
+    #TESTE DE ENVIO DE VALORES PARA O HTML
+    s_abc = [random.random() for _ in range(40)]
+    return render_template('index.html', s_abc=s_abc)
 
 @app.route('/perfil')
 def perfil():
@@ -13,12 +16,20 @@ def perfil():
 
 @app.route('/completar')
 def completar():
-    ...
+    return render_template('index.html', methods=['POST', 'GET'])
 
 @app.route('/traduzir')
 def traduzir():
-    ...
+    return render_template('traduzir.html', methods=['POST', 'GET'])
 
 @app.route('/ouvir')
 def ouvir():
     ...
+
+@app.route('/contato', methods=['POST', 'GET'])
+def contato():
+    return render_template("contato.html")
+
+@app.route('/testes', methods=['POST','GET'])
+def testes():
+    return render_template("testes.html")
